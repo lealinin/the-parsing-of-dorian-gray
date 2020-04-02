fd4920980a42eba8979306195b869a066a8f62a9
lealinin <lea.linin@gmail.com>
2020-04-02 12:19:06 -0700

    Import word_sentence_tokenize function and dorian_gray.txt

---
+++ script.py
@@ -0,0 +1,9 @@
+from nltk import pos_tag, RegexpParser
+from tokenize_words import word_sentence_tokenize # non-library functions
+
+# import text
+
+text = open("dorian_gray.txt", encoding='utf-8').read().lower()
+
+
+
