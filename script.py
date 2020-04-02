b22713d0364e6df29fc965fc1567e18429878659
lealinin <lea.linin@gmail.com>
2020-04-02 12:55:52 -0700

    Use word_sentence_tokenize function to create a word-sentence list

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 from nltk import pos_tag, RegexpParser
 from tokenize_words import word_sentence_tokenize # non-library functions
 
-# import text
-
 text = open("dorian_gray.txt", encoding='utf-8').read().lower()
+
+word_tokenized_text = word_sentence_tokenize(text)
+
+
 
 
 
